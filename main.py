234b4594235419cbdf5edd8ecb59808202febf8c
DR-1300 <dhruvi.rao90@gmail.com>
2025-05-19 18:23:27 +0530

    modified:   main.py 	new file:   options/new_parking.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+# Parking System
+import sys 
+sys.path.insert(0, 'options')
+from new_parking import new_parking
 print("-------------------Parking system-------------------")
 print("Welcome to the Parking system! What would you like to do today?")
 print("1. New parking")
 print("2. View vehicle")
+
+choice = input("Enter your choice (1 or 2): ")
+if choice == "1":
+    new_parking()
